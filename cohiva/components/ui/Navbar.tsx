cc8877cbe613cbd28c598d547712ba3acdbebff7
Fardin1023 <fardinkamran915@gmail.com>
2026-08-29 01:17:33 +0600

    Home Page Creation

---
+++ cohiva/components/ui/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="fixed z-50 flex w-full items-center justify-between bg-dark-1 px-6 py-4 text-white">
+      <h1 className="text-2xl font-bold text-light-1">
+        Cohiva
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
