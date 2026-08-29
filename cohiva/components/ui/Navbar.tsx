23b28db6def58bf4a7841a338b808a99b2754823
Fardin1023 <fardinkamran915@gmail.com>
2026-08-29 23:03:36 +0600

    Mobile Navbar updated

--- cohiva/components/ui/Navbar.tsx
+++ cohiva/components/ui/Navbar.tsx
@@ -4,11 +4,10 @@ import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
-    <nav className="fixed left-0 top-0 z-50 flex h-[84px] w-full items-center bg-[#B9687C] px-5 shadow-sm">
-      <Link
-        href="/"
-        className="flex items-center gap-3"
-      >
+    <nav className="fixed left-0 top-0 z-50 flex h-[84px] w-full items-center justify-between bg-[#B9687C] px-4 sm:px-6">
+      
+      {/* Left side - Logo + Cohiva */}
+      <Link href="/" className="flex items-center gap-3">
         <Image
           src="/images/CohivaLogo.png"
           alt="Cohiva logo"
@@ -22,10 +21,12 @@ const Navbar = () => {
           Cohiva
         </h1>
       </Link>
-      <div className="flex-between gap-5">
-        {/* User Management*/}
-        <MobileNav/>
+
+      {/* Right side - Hamburger */}
+      <div className="flex items-center">
+        <MobileNav />
       </div>
+
     </nav>
   );
 };
